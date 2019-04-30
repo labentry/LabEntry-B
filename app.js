b4bfb81264ca4353c0f0828321187b67dd7e7ec2
PulkitMehta94 <41365083+PulkitMehta94@users.noreply.github.com>
2019-04-30 21:17:26 +1000

    app js and package json with latest changes

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-/*eslint-env node*/
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
+var bodyParser = require('body-parser');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
 var Cloudant = require('@cloudant/cloudant');
@@ -19,70 +11,41 @@ var password = "28cb5df88d5cfe96404fd03ac81a88a531e8103ee079b429cda0adff9a58f8a0
 var cloudant = Cloudant({ account: me, password: password });
 cloudant.db.list(function (err, allDbs) {
   console.log('All my database is:$s', allDbs.join(','));
-  
 });
-//test
-// async function asyncCall() {
-//   await cloudant.db.create('alice');
-//   return cloudant.use('alice').insert({ happy: true }, 'rabbit');
-// }
+// create a new express server
+var app = express();
 
-// asyncCall().then((data) => {
-//   console.log("Data is:");
-//   console.log(data); // { ok: true, id: 'rabbit', ...
-// }).catch((err) => {
-//   console.log(err);
-// });
-
-
-// Using Promises.
-
-// cloudant.db.create('alice').then(() => {
-//   cloudant.use('alice').insert({ happy: true }, 'rabbit').then((data) => {
-//     console.log(data); // { ok: true, id: 'rabbit', ...
-//   });
-// }).catch((err) => {
-//   console.log(err);
-// });
-
-// Using Callbacks.
-
-// cloudant.db.create('alice', (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     cloudant.use('alice').insert({ happy: true }, 'rabbit', (err, data) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(data); // { ok: true, id: 'rabbit', ...
-//       }
-//     });
-//   }
-// });
-  // create a new express server
-  var app = express();
-
-  app.get('/test',function(req,res)
-  {
-    var data= req.body;
-    
-    var i=cloudant.db.use('test');
-    i.get("4d3defc6c0b4a6cd6450aba39e21d8a9",function(err,data){
-      console.log("Data:", data);
-      res.send(data);
-    });
-    
-    
-  })
-  // serve the files out of ./public as our main files
-  app.use(express.static(__dirname + '/public'));
-
-  // get the app environment from Cloud Foundry
-  var appEnv = cfenv.getAppEnv();
-
-  // start server on the specified port and binding host
-  app.listen(appEnv.port, '0.0.0.0', function () {
-    // print a message when the server starts listening
-    console.log("server starting on " + appEnv.url);
+app.get('/getStudentsData', function (req, res) {
+  var i = cloudant.db.use('students_data');
+  i.get("students_data", function (err, data) {
+    console.log("Data:", data);
+    res.send(data);
   });
+
+
+})
+app.get('/getLoginData', function (req, res) {
+  var i = cloudant.db.use('login_data');
+  i.get("login_data", function (err, data) {
+    console.log("Data:", data);
+    res.send(data);
+  });
+})
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/unitsInfo', function (req, res) {
+  console.log(__dirname + '/public');
+  res.sendFile(__dirname + '/public/pages/unitsInfo.html');
+  console.log("/login working");
+})
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function () {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
