610bddc7ee95bfcf444803b7be6744b648618022
PulkitMehta94 <41365083+PulkitMehta94@users.noreply.github.com>
2019-05-21 12:09:33 +1000

    Final Changes added
    Css changes and functionality changes added.
    Trimester and year also added in the data selection and viewing part.

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -5,23 +5,27 @@ $(document).ready(function () {
         console.log(data);
     });
     
-});
-function checkLoginDetails(username, password) {
-    console.log(data.length);
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].user == username.value) {
-            if (data[i].password == password.value) {
-                console.log("Successfully Logged in");
-                window.location.href = '/unitsInfo';
-
+    $("#login_form").submit(function(e){
+        e.preventDefault();
+        var username= document.getElementById("username").value;
+        var password= document.getElementById("password").value;
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].user == username) {
+                if (data[i].password == password) {
+                    console.log("Successfully Logged in");
+                    window.location.href = '/unitsInfo';
+    
+                }
+                else {
+                    console.log("Either Wrong username or password");
+                }
             }
-            else {
-                console.log("Either Wrong username or password");
-            }
+    
         }
+    });
 
-    }
-}
+});
+
 function callApi() {
     // $.ajax({
     //     url: 'https://anypoint.mulesoft.com/mocking/api/v1/links/8916e3ea-2e5d-4fdc-8a75-951ea62b9ef2/assessmentResults',
@@ -40,15 +44,11 @@ function callApi() {
     // }); 
     var settings = {
         "async": true,
-        "crossDomain": true,
         "url": "https://anypoint.mulesoft.com/mocking/api/v1/links/8916e3ea-2e5d-4fdc-8a75-951ea62b9ef2/assessmentResults?unitCode=SIT740&studentId=56127843",
         "method": "GET",
         "headers": {
           "client_id": "abc",
-          "client_secret": "abc",
-          "cache-control": "no-cache",
-          "postman-token": "d8adc39e-8b04-ee20-517a-d4cee198fb51",
-          "Access-Control-Allow-Origin":"*"
+          "client_secret": "abc"
         }
       }
       
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,238 +1,198 @@
 var data;
-$(document).ready(function(){
-    $.get('/getStudentsData',function(result){
-        data=result.student;
-        console.log(result.student);
+$(document).ready(function () {
+    $.get('/getStudentsData', function (result) {
+        data = result.students;
+        console.log(result.students);
     });
     document.getElementById('txtFileUpload').addEventListener('change', upload, false);
 
-    $("#myInput").on("keyup", function() {
+    $("#myInput").on("keyup", function () {
         var value = $(this).val().toLowerCase();
-        $("#myTable tr").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        $("#myTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
-      });
+    });
     // Method that checks that the browser supports the HTML5 File API
     function browserSupportFileUpload() {
         var isCompatible = false;
         if (window.File && window.FileReader && window.FileList && window.Blob) {
-        isCompatible = true;
+            isCompatible = true;
         }
         return isCompatible;
     }
+    $("#form1").submit(function (e) {
+        e.preventDefault();
+        var unit = document.getElementById("Unit1").value;
+        var tri = document.getElementById("tri").value;
+        var year = document.getElementById("year").value;
+        if (unit != "" && tri != "" && year != "") {
+            console.log("Unit Selected:", unit);
+            displayStudents(unit, tri, year);
+        }
+        else {
+            alert("Please select a unit");
+            console.log("Unit is not selected");
+        }
+    })
 
+    $("#eqn-bg").click(function (e) {
+        e.preventDefault();
+        console.log("CheckID");
+        var unit = document.getElementById("Unit2").value;
+        var studentID = document.getElementById("result").innerText;
+        console.log(document.getElementById("Unit2").value);
+        console.log(document.getElementById("result").innerText);
+        if (unit != "" || studentID != "") {
+            // AJAX call
+            //if else 
+            alert("Student has completed the pre-requisites for the unit:" + unit);
+        }
+    })
     // Method that reads and processes the selected file
     function upload(evt) {
-    if (!browserSupportFileUpload()) {
-        alert('The File APIs are not fully supported in this browser!');
+        if (!browserSupportFileUpload()) {
+            alert('The File APIs are not fully supported in this browser!');
         } else {
             var data = null;
             var file = evt.target.files[0];
             var reader = new FileReader();
             reader.readAsText(file);
-            reader.onload = function(event) {
-                var csvData = event.target.result;
-                data = $.csv.toArrays(csvData);
-                if (data && data.length > 0) {
-                  alert('Imported -' + data.length + '- rows successfully!');
-                  console.log(data);
-                } else {
-                    alert('No data to import!');
+            reader.onload = function (event) {
+                var csv = event.target.result;
+                console.log(csv);
+                var lines = csv.split("\n");
+
+                var result = [];
+
+                var headers = lines[0].split(",");
+
+                for (var i = 1; i < lines.length; i++) {
+
+                    var obj = {};
+                    var currentline = lines[i].split(",");
+
+                    for (var j = 0; j < headers.length; j++) {
+                        obj[headers[j]] = currentline[j];
+                    }
+
+                    result.push(obj);
+
                 }
+
+                //return result; //JavaScript object
+                console.log(result); //JSON
+                // data = $.csv.toArrays(csvData);
+                // if (data && data.length > 0) {
+                //   alert('Imported -' + data.length + '- rows successfully!');
+                //   console.log(data);
+                // } else {
+                //     alert('No data to import!');
+                // }
             };
-            reader.onerror = function() {
+            reader.onerror = function () {
                 alert('Unable to read ' + file.fileName);
             };
         }
     }
- //checkID
- var current,
+    //checkID
+    var current,
         screen,
         output,
         limit,
         zero,
         period,
         operator;
-        
-        screen = document.getElementById("result");
-    
+
+    screen = document.getElementById("result");
+
     var elem = document.querySelectorAll(".num");
-        
-          var len = elem.length;
-        
-          for(var i = 0; i < len; i++ ) {
-            
-            elem[i].addEventListener("click",function() {
-                output=screen.innerHTML;
-                limit = output.length;
-                if(limit<10){
-                      
+
+    var len = elem.length;
+
+    for (var i = 0; i < len; i++) {
+
+        elem[i].addEventListener("click", function () {
+            output = screen.innerHTML;
+            limit = output.length;
+            if (limit < 10) {
+
                 num = this.value;
-                         
-                output = screen.innerHTML +=num;
-                      
-                }
-             if(limit > 10 ) {
-            
-             alert("Sorry no more input is allowed");
-                 
-           }
-           
-         },false);
-            
-        } 
-    
-        document.querySelector(".zero").addEventListener("click",function() {
-            
-            zero = this.value;
-            
-            if(screen.innerHTML === "") {
-                
-               output = screen.innerHTML = zero;  
+
+                output = screen.innerHTML += num;
+
             }
-            
-            else if(screen.innerHTML === output) {
-                
-             output = screen.innerHTML +=zero;
-                
+            if (limit > 10) {
+
+                alert("Sorry no more input is allowed");
+
             }
-              
-        },false);
-        
-        // document.querySelector(".period").addEventListener("click",function() {
-            
-        //     period = this.value;
-            
-        //     if(screen.innerHTML === "") {
-                
-        //      output = screen.innerHTML = screen.innerHTML.concat("0.");
-                
-        //      }
-        
-        //     else if(screen.innerHTML === output) {
-            
-        //       screen.innerHTML = screen.innerHTML.concat(".");
-                
-        //     }
-            
-        // },false);
-        
-        
-        // document.querySelector("#eqn-bg").addEventListener("click",function() {
-            
-        //   if(screen.innerHTML === output) {
-              
-        //     screen.innerHTML = eval(output);
-            
-        //     output = screen.innerHTML;
-            
-        //   }
-            
-        //   else {
-        //         screen.innerHTML = "";
-        //   }
-              
-        // },false);
-        
-     document.querySelector("#delete").addEventListener("click",function() {
-            
-            screen.innerHTML = "";
-            
-        },false);
-        
-       
-        //  var elem1 = document.querySelectorAll(".operator");
-        
-        //   var len1 = elem1.length;
-        
-        //   for(var i = 0; i < len1; i++ ) {
-            
-        //     elem1[i].addEventListener("click",function() {
-             
-        //     operator = this.value;
-             
-        //      if(screen.innerHTML === "") {
-                
-        //         screen.innerHTML = screen.innerHTML.concat("");
-                
-        //     }
-            
-        //     else if(output) {
-            
-        //         screen.innerHTML = output.concat(operator);
-                
-        //     }
-               
-        // },false);
-              
-        //   } 
+
+        }, false);
+
+    }
+
+    document.querySelector(".zero").addEventListener("click", function () {
+
+        zero = this.value;
+
+        if (screen.innerHTML === "") {
+
+            output = screen.innerHTML = zero;
+        }
+
+        else if (screen.innerHTML === output) {
+
+            output = screen.innerHTML += zero;
+
+        }
+
+    }, false);
+
+
+    document.querySelector("#delete").addEventListener("click", function () {
+        var del = screen.innerHTML;
+        del = del.substring(0, del.length - 1);
+        screen.innerHTML = del;
+
+    }, false);
+
+
+
 });
 
-function unitSelected(unit)
-{   
-    if(unit!="")
-    {
-        console.log("Unit Selected:",unit);
-        displayStudents(unit);
-    }
-    else
-    {
-    alert("Please select a unit");
-    console.log("Unit is not selected");
-    }
-}
-function displayStudents(unit)
-{   
-    var  studentTable="<table id='datatable'><tr><td style='width: 80px; color: white; text-align: center'>STUDENT ID</td>";
-    studentTable+= "<td style='width: 80px; color: white; text-align: center;'>STUDENT NAME</td>";
-    studentTable+="<td style='width: 80px; color: white; text-align: center;'>UNIT ENROLLED</td>";
-    studentTable+="<td style='width: 80px; color: white; text-align: center;'>PRE-REQUISITE\n(COMPLETED)</td></tr>";
-    studentTable+="<tbody id='myTable'>";
 
-    if(data.length==0)
-    {
+function displayStudents(unit, tri, year) {
+    var studentTable = "<table id='datatable'><tr><td style='width: 80px;background-color: #13554b; text-align: center'>STUDENT ID</td>";
+    studentTable += "<td style='width: 80px; text-align: center;background-color: #13554b;'>STUDENT NAME</td>";
+    studentTable += "<td style='width: 80px;  text-align: center;background-color: #13554b;'>UNIT ENROLLED</td>";
+    studentTable += "<td style='width: 80px;  text-align: center;background-color: #13554b;'>YEAR</td>";
+    studentTable += "<td style='width: 80px;  text-align: center;background-color: #13554b;'>TRIMESTER</td>";
+    studentTable += "<td style='width: 80px;  text-align: center;background-color: #13554b;'>PRE REQUISITE COMPLETED</td></tr>";
+    studentTable += "<tbody id='myTable'>";
+
+    if (data.length == 0) {
         console.log("No Data");
     }
 
-    
-    else
-    {
-     
-    for (var i=0; i<data.length; i++) 
-    {
-        if(data[i].unit_enrolled==unit)
-        {
-            studentTable+="<tr><td style='width: 80px; text-align: center;'>"+data[i].id+"</td>";
-            studentTable+="<td     style='width: 80px; text-align: center;'>"+data[i].name+"</td>";
-            studentTable+="<td     style='width: 80px; text-align: center;'>"+data[i].unit_enrolled+"</td>";
-            studentTable+="<td     style='width: 80px; text-align: center;'>"+data[i].preRequisite+"</td></tr>";
+
+    else {
+
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].unit_enrolled == unit && data[i].year == year && data[i].trimester == tri) {
+                studentTable += "<tr><td style='width: 80px; text-align: center;'>" + data[i].id + "</td>";
+                studentTable += "<td     style='width: 80px; text-align: center;'>" + data[i].name + "</td>";
+                studentTable += "<td     style='width: 80px; text-align: center;'>" + data[i].unit_enrolled + "</td>";
+                studentTable += "<td     style='width: 80px; text-align: center;'>" + data[i].year + "</td>";
+                studentTable += "<td     style='width: 80px; text-align: center;'>" + data[i].trimester + "</td>";
+                studentTable += "<td     style='width: 80px; text-align: center;'>" + data[i].preRequisite + "</td></tr>";
+            }
         }
-    } 
-    
-    studentTable+="</tbody></table>"
-    
+
+        studentTable += "</tbody></table>"
+
     }
-    document.getElementById("studentTable").innerHTML=studentTable;
-    document.getElementById("viewTable").style.display="table-cell";
+    document.getElementById("studentTable").innerHTML = studentTable;
+    document.getElementById("viewTable").style.display = "table-cell";
 }
-// check ID
 
-// window.onload = function() {
 
-      
-//     }
-
-function checkID()
-{   console.log("CheckID");
-    var unit=document.getElementById("Unit").value;
-    var studentID=document.getElementById("result").innerText;
-    console.log(document.getElementById("Unit").value);
-    console.log(document.getElementById("result").innerText);
-    if (unit!=""||studentID!="")
-    {
-        // AJAX call
-        //if else 
-        alert("Student has completed the pre-requisites for the unit:"+unit);
-    }
-    
-}
