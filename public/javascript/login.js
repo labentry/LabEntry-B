595eba0c70c9b9d456f061692a30883d88652f8e
balachand1 <41718492+balachand1@users.noreply.github.com>
2019-05-25 18:03:38 +1000

    Update login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,29 +1,4 @@
-var data;
-$(document).ready(function () {
-    // Brings the username and password from db
-    $.get('/getLoginData', function (result) {
-        data = result.users;
-        console.log(data);
-    });
-    // Form to submit
-    $("#login_form").submit(function(e){
-        e.preventDefault();
-        var username= document.getElementById("username").value;
-        var password= document.getElementById("password").value;
-        var check;
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].user == username) {
-                check=true;
-                if (data[i].password == password) {
-                    console.log("Successfully Logged in");
-                    window.location.href = '/unitsInfo';
-                }
-                else {
-                    document.getElementById("error").style.display="Block";
-                    console.log("Either Wrong username or password");
-                }
-            }
-        }
+
         if(!check) {
             document.getElementById("error").style.display="Block";
             
